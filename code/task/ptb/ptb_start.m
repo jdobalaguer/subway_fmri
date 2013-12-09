dd8ead593f04b2307d7308b6e24c82902dbf1a37
jan <jan@deepmind.com>
2013-12-09 15:22:42 +0000

    added gamepad mode to read triggers/buttonbox; bigger fontsize; ready for training_1

--- code/task/ptb/ptb_start.m
+++ code/task/ptb/ptb_start.m
@@ -17,11 +17,14 @@ else
 end
 
 %% Screen
+% list screens
+ptb.screen_ii = Screen('Screens');
+ptb.screen_i = ptb.screen_ii(end);
 % open window
 if isfield(parameters,'screen_rect') && ~isempty(parameters.screen_rect)
-    [tmp_w, tmp_r] = Screen('OpenWindow', 0, 0,parameters.screen_rect,32,2);
+    [tmp_w, tmp_r] = Screen('OpenWindow', ptb.screen_i, 0,parameters.screen_rect,32,2);
 else
-    [tmp_w, tmp_r] = Screen('OpenWindow', 0, 0,[],32,2);
+    [tmp_w, tmp_r] = Screen('OpenWindow', ptb.screen_i, 0,[],32,2);
 end
 
 % values
@@ -39,13 +42,6 @@ Screen(ptb.screen_w,'BlendFunction',GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 % mouse
 HideCursor;
-
-
-%% Keyboard
-
-% unify names
-KbName('UnifyKeyNames');
-
 
 %% Audio
 
@@ -67,6 +63,25 @@ if parameters.flag_audio
     tmp_s = PsychPortAudio('GetStatus',ptb.audio_port);
     ptb.audio_sampling = tmp_s.SampleRate;
 end
+
+%% Gamepad
+
+Gamepad('Unplug');
+ptb.gamepad_i = Gamepad('GetNumGamepads');
+ptb.gamepad_name = '';
+ptb.gamepad_nbbuttons = 0;
+if ptb.gamepad_i>0
+    ptb.gamepad_name      = Gamepad('GetGamepadNamesFromIndices',ptb.gamepad_i);
+    ptb.gamepad_nbbuttons = Gamepad('GetNumButtons',ptb.gamepad_i);
+end
+
+%% Keyboard
+
+% select the right keyboard
+ptb.kb_ii = GetKeyboardIndices();
+ptb.kb_i  = ptb.kb_ii(end);
+% unify names
+KbName('UnifyKeyNames');
 
 %% Textures
 [ptb.texture_name,ptb.texture_id] = ptb_maketextures(ptb.screen_w,parameters.screen_picture.alpha);
