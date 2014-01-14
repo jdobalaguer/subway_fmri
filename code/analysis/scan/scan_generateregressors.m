e75290d3c55feb6d32992c9cffde20e42cf2e359
jan <jan@deepmind.com>
2014-01-14 19:48:26 +0000

    added factors to the scan_process analysis

--- code/analysis/scan/scan_generateregressors.m
+++ code/analysis/scan/scan_generateregressors.m
@@ -26,10 +26,10 @@ function [timeruns,dataruns] = scan_generateregressors(datafiles)
         datafile.time.offset = datafile.time.getsecs - datafile.time.breakgs;
 
         % set run template
-        run_template = struct();
+        timerun_template = struct();
         u_screen = unique(datafile.time.screens);
         for i_screen = 1:length(u_screen)
-            run_template.(['screen_',u_screen{i_screen}]) = [];
+            timerun_template.(['screen_',u_screen{i_screen}]) = [];
         end
         
         % map faces
@@ -49,12 +49,12 @@ function [timeruns,dataruns] = scan_generateregressors(datafiles)
             
             %% add screens
             % load template
-            run = run_template;
+            timerun = timerun_template;
             % set index
             ii_trial = ii_break(i_break):(ii_break(i_break+1)-1);
             % set trial
             for i_trial = 1:length(ii_trial)
-                run.(['screen_',datafile.time.screens{ii_trial(i_trial)}])(end+1) = datafile.time.offset(ii_trial(i_trial));
+                timerun.(['screen_',datafile.time.screens{ii_trial(i_trial)}])(end+1) = datafile.time.offset(ii_trial(i_trial));
             end
             
             %% add trial data
@@ -66,23 +66,23 @@ function [timeruns,dataruns] = scan_generateregressors(datafiles)
             % exchange
             ii_exchange = logical([datafile.map.stations(datafile.data.avatar_instation).exchange]);
             time_exchange = trial_os(ii_run & ii_exchange);
-            run.('avatar_exchange') = time_exchange;
+            timerun.('avatar_exchange') = time_exchange;
             
             % elbow
             ii_elbow = logical([datafile.map.stations(datafile.data.avatar_instation).elbow]);
             time_elbow = trial_os(ii_run & ii_elbow);
-            run.('avatar_elbow') = time_elbow;
+            timerun.('avatar_elbow') = time_elbow;
             
             % regular
             time_regular = trial_os(ii_run & ~ii_exchange);
-            run.('avatar_regular') = time_regular;
+            timerun.('avatar_regular') = time_regular;
             
             % face/place
             ii_face  = logical([datafile.map.stations(datafile.data.avatar_instation).face]);
             time_face = trial_os(ii_run & ii_face);
             time_place = trial_os(ii_run & ~ii_face);
-            run.('avatar_face') = time_face;
-            run.('avatar_place') = time_place;
+            timerun.('avatar_face') = time_face;
+            timerun.('avatar_place') = time_place;
             
             % switch / no switch (in exchange stations)
             ii_diffline = logical([diff(datafile.data.avatar_insubline),0]);
@@ -90,48 +90,48 @@ function [timeruns,dataruns] = scan_generateregressors(datafiles)
             ii_switch = ii_exchange & ii_diffline & ~ii_stop;
             time_switch = trial_os(ii_run & ii_switch);
             time_noswitch = trial_os(ii_run & ~ii_switch);
-            run.('avatar_switch') = time_switch;
-            run.('avatar_noswitch') = time_noswitch;
+            timerun.('avatar_switch') = time_switch;
+            timerun.('avatar_noswitch') = time_noswitch;
             
             % forwards/backwards
             ii_start = datafile.data.exp_starttrial;
             ii_backwards = ii_diffline & ~ii_stop & ~ii_exchange & ~ii_start;
             time_forward   = trial_os(ii_run & ~ii_backwards);
             time_backwards = trial_os(ii_run &  ii_backwards);
-            run.('avatar_forward'  ) = time_forward;
-            run.('avatar_backwards') = time_backwards;
+            timerun.('avatar_forward'  ) = time_forward;
+            timerun.('avatar_backwards') = time_backwards;
             
             % inertia/exertia
             ii_exertia = ii_diffline | ii_elbow | ii_switch | ii_backwards;
             time_inertia = trial_os(ii_run & ~ii_exertia);
             time_exertia = trial_os(ii_run &  ii_exertia);
-            run.('avatar_inertia') = time_inertia;
-            run.('avatar_exertia') = time_exertia;
+            timerun.('avatar_inertia') = time_inertia;
+            timerun.('avatar_exertia') = time_exertia;
             
             % achieved/bailout
             ii_togoal = (datafile.data.avatar_goalstation(ii_run & ii_stop)==datafile.data.resp_station(ii_run & ii_stop));
-            time_achieved = run.screen_rew(ii_togoal(1:length(run.screen_rew)));
-            time_bailout = run.screen_rew(~ii_togoal(1:length(run.screen_rew)));
-            run.('avatar_achieved') = time_achieved;
-            run.('avatar_bailout') = time_bailout;
+            time_achieved = timerun.screen_rew(ii_togoal(1:length(timerun.screen_rew)));
+            time_bailout = timerun.screen_rew(~ii_togoal(1:length(timerun.screen_rew)));
+            timerun.('avatar_achieved') = time_achieved;
+            timerun.('avatar_bailout') = time_bailout;
             
             %% remove fields
-            run = rmfield(run,'screen_blank');
-            run = rmfield(run,'screen_blockpos');
-            run = rmfield(run,'screen_blockpre');
-            run = rmfield(run,'screen_breakpos');
-            run = rmfield(run,'screen_breakpre');
-            run = rmfield(run,'screen_breakwait');
-            run = rmfield(run,'screen_clickpress');
-            run = rmfield(run,'screen_clickrelease');
-            run = rmfield(run,'screen_nan');
-            run = rmfield(run,'screen_trialpress');
-            run = rmfield(run,'screen_endblank');
-            run = rmfield(run,'screen_endtext');
-            run = rmfield(run,'screen_lottery');
+            timerun = rmfield(timerun,'screen_blank');
+            timerun = rmfield(timerun,'screen_blockpos');
+            timerun = rmfield(timerun,'screen_blockpre');
+            timerun = rmfield(timerun,'screen_breakpos');
+            timerun = rmfield(timerun,'screen_breakpre');
+            timerun = rmfield(timerun,'screen_breakwait');
+            timerun = rmfield(timerun,'screen_clickpress');
+            timerun = rmfield(timerun,'screen_clickrelease');
+            timerun = rmfield(timerun,'screen_nan');
+            timerun = rmfield(timerun,'screen_trialpress');
+            timerun = rmfield(timerun,'screen_endblank');
+            timerun = rmfield(timerun,'screen_endtext');
+            timerun = rmfield(timerun,'screen_lottery');
             
             %% save run
-            timeruns{end+1} = run;
+            timeruns{end+1} = timerun;
         end
     end
 end
--- code/analysis/scan/scan_process.m
+++ code/analysis/scan/scan_process.m
@@ -15,20 +15,21 @@ function scan_process()
     % data
     dir_datsubs                    = [pwd(),filesep,'data',filesep,'data',filesep,'scanner',filesep];
     dir_process                    = [pwd(),filesep,'data',filesep,'process',filesep,'process',filesep];
-    dir_datregs                    = [dir_process,'regressors',filesep];
+    dir_datcons                    = [dir_process,'conditions',filesep];
     dir_datglm1s                   = [dir_process,'firstlevel',filesep];
     dir_datglm2s                   = [dir_process,'secondlevel',filesep];
     file_datsubs                   = dir([dir_datsubs,'data_*.mat']); file_datsubs = strcat(dir_datsubs,strvcat(file_datsubs.name));
-    file_datregs                   = dir([dir_datregs,'regressors_*_reg.mat']); file_datregs = strcat(dir_datregs,strvcat(file_datregs.name));
-    file_datreas                   = dir([dir_datregs,'regressors_*_rea.mat']); file_datreas = strcat(dir_datregs,strvcat(file_datreas.name));
+    file_datcons                   = dir([dir_datcons,'condition_*.mat']); file_datcons = strcat(dir_datcons,strvcat(file_datcons.name));
+    file_datreas                   = dir([dir_datcons,'realign_*.mat']); file_datreas = strcat(dir_datcons,strvcat(file_datreas.name));
     
     % VARIABLES
     nb_subjects = size(dir_niisubs, 1);
     nb_runs     = size(dir_niiruns, 1);
-    u_subject   = [1,2]; %1:nb_subjects;
+    u_subject   = 1:nb_subjects;
     u_run       = 1:nb_runs;
     u_regressor = {};
     u_modulator = {};
+    u_factor    = {};
     u_contrast  = {};
     eval(fileread([dir_process,'process.m']));
     
@@ -37,13 +38,13 @@ function scan_process()
     pars_voxs    = 4;
     
     % FLAGS
-    do_all  = true;
-    do_regs = do_all || ~exist(dir_datregs ,'file');
+    do_all  = false;
+    do_regs = do_all || ~exist(dir_datcons ,'file');
     do_frst = do_all || ~exist(dir_datglm1s,'file');
     do_scnd = do_all || true;
     
     %% DELETE
-    if do_regs && exist(dir_datregs ,'dir'); rmdir(dir_datregs ,'s'); end
+    if do_regs && exist(dir_datcons ,'dir'); rmdir(dir_datcons ,'s'); end
     if do_frst && exist(dir_datglm1s,'dir'); rmdir(dir_datglm1s,'s'); end
     if do_scnd && exist(dir_datglm2s,'dir'); rmdir(dir_datglm2s,'s'); end
     
@@ -62,42 +63,45 @@ function scan_process()
     function build_regressors()
         if ~do_regs; return; end
         % make directory
-        if ~exist(dir_datregs,'dir'); mkdirp(dir_datregs); end
+        if ~exist(dir_datcons,'dir'); mkdirp(dir_datcons); end
         % load regressors
-        [timeruns,dataruns] = scan_loadregressors('scanner',u_subject);
+        [timeregs,dataregs] = scan_loadregressors('scanner');
+        [timefacs,datafacs] = scan_loadfactors('scanner');        
         j_run = 0;
         for i_sub = u_subject
             dir_niiepi3 = strtrim(dir_niiepis3(i_sub,:));
             fprintf('Building regressors for: %s\n',dir_niiepi3);
             for i_run = u_run
-                file_datreg = sprintf('%sregressors_sub_%02i_run_%02i_reg.mat',dir_datregs,i_sub,i_run);
-                file_datrea = sprintf('%sregressors_sub_%02i_run_%02i_rea.mat',dir_datregs,i_sub,i_run);
+                file_datcon = sprintf('%scondition_sub_%02i_run_%02i.mat',dir_datcons,i_sub,i_run);
+                file_datrea = sprintf('%srealign_sub_%02i_run_%02i.mat',  dir_datcons,i_sub,i_run);
                 % index
                 j_run = j_run+1;
-                assert(j_run<=length(timeruns),'scan_process: build_regressors: error 1.');
+                assert(j_run<=length(timeregs),'scan_process: build_regressors: error 1.');
                 % dirs & files
                 dir_niirun     = strcat(dir_niiepi3,strtrim(dir_niiruns(i_run,:)));
                 dir_niiimg     = strcat(dir_niirun,'images',filesep);
                 file_niirea    = dir([dir_niiimg,'rp_image*.txt']);                       file_niirea = strcat(dir_niiimg,strvcat(file_niirea.name));
                 file_niiimg    = dir([dir_niiimg,sprintf('sw%duimages*.nii',pars_voxs)]); file_niiimg = strcat(dir_niiimg,strvcat(file_niiimg.name));
-                % create regressors
-                reg = {};
+                % create conditions
+                % regressor
+                cond = {};
                 for i_reg = 1:length(u_regressor)
-                    onset = get_regonset(timeruns{j_run},u_regressor{i_reg});
-                    reg{end+1} = struct('name',     {u_regressor{i_reg}.name}, ...
-                                        'onset',    {onset}                   , ...
-                                        'duration', {0}                       );
+                    onset = get_regonset(timeregs{j_run},u_regressor{i_reg});
+                    cond{end+1} = struct('name',     {u_regressor{i_reg}.name}, ...
+                                         'onset',    {onset}                  , ...
+                                         'subname',  {[]}                     , ...
+                                         'level',    {[]}                     , ...
+                                         'duration', {0}                      );
                 end
-                % create modulators
-                mod = {};
+                % modulator
                 for i_mod1 = 1:length(u_modulator)
-                    onset = get_modonset(timeruns{j_run},u_modulator{i_mod1});
+                    onset = get_modonset(timeregs{j_run},u_modulator{i_mod1});
                     subnames = {};
                     levels   = {};
                     for i_mod2 = 1:length(u_modulator{i_mod1}.value)
                         level = zeros(size(onset));
                         for i_mod3 = 1:length(u_modulator{i_mod1}.value{i_mod2}.value)
-                            onset_level = timeruns{j_run}.(u_modulator{i_mod1}.value{i_mod2}.value{i_mod3});
+                            onset_level = timeregs{j_run}.(u_modulator{i_mod1}.value{i_mod2}.value{i_mod3});
                             ii_level    = ismember(onset,onset_level);
                             v_level     = u_modulator{i_mod1}.value{i_mod2}.level(i_mod3);
                             level(ii_level) = v_level;
@@ -105,22 +109,39 @@ function scan_process()
                         subnames{i_mod2} = u_modulator{i_mod1}.value{i_mod2}.name;
                         levels{i_mod2}   = level;
                     end
-                    mod{end+1} = struct('name' ,    {u_modulator{i_mod1}.name}, ...
-                                        'onset',    {onset}                   , ...
-                                        'subname',  {subnames}                 , ...
-                                        'level',    {levels}                   , ...
-                                        'duration', {0}                       );
+                    cond{end+1} = struct('name' ,    {u_modulator{i_mod1}.name}, ...
+                                         'onset',    {onset}                   , ...
+                                         'subname',  {subnames}                , ...
+                                         'level',    {levels}                  , ...
+                                         'duration', {0}                       );
                 end
+                % factor
+                for i_fac1 = 1:length(u_factor)
+                    onset = timefacs{j_run};
+                    subnames = {};
+                    levels   = {};
+                    for i_fac2 = 1:length(u_factor{i_fac1}.value)
+                        subnames{i_fac2} = u_factor{i_fac1}.value{i_fac2}.name;
+                        levels{i_fac2}   = datafacs{j_run}.(u_factor{i_fac1}.value{i_fac2}.value);
+                    end
+                    cond{end+1} = struct('name' ,    {u_factor{i_fac1}.name}, ...
+                                         'onset',    {onset}                , ...
+                                         'subname',  {subnames}             , ...
+                                         'level',    {levels}               , ...
+                                         'duration', {0}                    );
+                end
+                % merge conditions
+                cond = merge_cond(cond);
                 % load realignment
                 R = load(file_niirea);
                 % save regressors
-                save(file_datreg,'reg','mod');
+                save(file_datcon,'cond');
                 save(file_datrea,'R');
             end
         end
         % update file_datregs
-        file_datregs = dir([dir_datregs,'regressors_*_reg.mat']); file_datregs = strcat(dir_datregs,strvcat(file_datregs.name));
-        file_datreas = dir([dir_datregs,'regressors_*_rea.mat']); file_datreas = strcat(dir_datregs,strvcat(file_datreas.name));
+        file_datcons = dir([dir_datcons,'condition_*.mat']); file_datcons = strcat(dir_datcons,strvcat(file_datcons.name));
+        file_datreas = dir([dir_datcons,'realign_*.mat']);   file_datreas = strcat(dir_datcons,strvcat(file_datreas.name));
     end
     
     %% GLM FIRST LEVEL
@@ -149,19 +170,14 @@ function scan_process()
                 dir_niirun = strcat(dir_niiepi3,strtrim(dir_niiruns(i_run,:)));
                 dir_niiimg = strcat(dir_niirun,'images',filesep);
                 file_niiimg = cellstr(spm_select('FPlist', dir_niiimg,['^',sprintf('sw%duimages',pars_voxs),'.*\.nii']));
-                file_datreg = strtrim(file_datregs(j_run,:));
+                file_datcon = strtrim(file_datcons(j_run,:));
                 file_datrea = strtrim(file_datreas(j_run,:));
-                loadreg = load(file_datreg,'reg','mod');
+                loadcond = load(file_datcon,'cond');
                 % truncate
                 nb_ons = 0;
-                for i_reg = 1:length(loadreg.reg)
-                    if (nb_ons < max(loadreg.reg{i_reg}.onset)/pars_tr)
-                        nb_ons = max(loadreg.reg{i_reg}.onset)/pars_tr;
-                    end
-                end
-                for i_mod = 1:length(loadreg.mod)
-                    if (nb_ons < max(loadreg.mod{i_mod}.onset)/pars_tr)
-                        nb_ons = max(loadreg.mod{i_mod}.onset)/pars_tr;
+                for i_cond = 1:length(loadcond.cond)
+                    if (nb_ons < max(loadcond.cond{i_cond}.onset)/pars_tr)
+                        nb_ons = max(loadcond.cond{i_cond}.onset)/pars_tr;
                     end
                 end
                 nb_ons = ceil(nb_ons);
@@ -171,35 +187,23 @@ function scan_process()
                 job.spm.stats.fmri_spec.sess(i_run).scans = file_niiimg;
                 job.spm.stats.fmri_spec.sess(i_run).hpf = 128;
                 job.spm.stats.fmri_spec.sess(i_run).cond = struct('name',{},'onset',{},'duration',{},'tmod',{},'pmod',{});
-                % regressors
-                for i_reg = 1:length(loadreg.reg)
-                    tmp_onsets = loadreg.reg{i_reg}.onset;
-                    nb_rmons = sum(tmp_onsets/pars_tr > nb_min);
-                    if nb_rmons; fprintf('    warning: (%02i,%02i,%02i) delete %02i reg onsets \n',i_sub,i_run,i_reg,nb_rmons); end
-                    tmp_onsets(tmp_onsets/pars_tr > nb_min) = [];
+                % conditions (regressors, modulators & factors)
+                for i_cond1 = 1:length(loadcond.cond)
+                    % remove onsets out of boundary
+                    nb_rmons = sum(loadcond.cond{i_cond1}.onset/pars_tr > nb_min);
+                    if nb_rmons; fprintf('    warning: (%02i,%02i,%02i) delete %02i onsets \n',i_sub,i_run,i_mod,nb_rmons); end
+                    loadcond.cond{i_cond1}.onset(loadcond.cond{i_cond1}.onset/pars_tr > nb_min) = [];
+                    % save condition
                     cond = struct();
-                    cond.name     = loadreg.reg{i_reg}.name;
-                    cond.onset    = tmp_onsets;
-                    cond.duration = loadreg.reg{i_reg}.duration;
+                    cond.name     = loadcond.cond{i_cond1}.name;
+                    cond.onset    = loadcond.cond{i_cond1}.onset;
+                    cond.duration = loadcond.cond{i_cond1}.duration;
                     cond.tmod     = 0;
                     cond.pmod     = struct('name', {}, 'param', {}, 'poly', {});
-                    job.spm.stats.fmri_spec.sess(i_run).cond(end+1) = cond;
-                end
-                % modulators
-                for i_mod1 = 1:length(loadreg.mod)
-                    tmp_onsets = loadreg.mod{i_mod1}.onset;
-                    nb_rmons = sum(tmp_onsets/pars_tr > nb_min);
-                    if nb_rmons; fprintf('    warning: (%02i,%02i,%02i) delete %02i mod onsets \n',i_sub,i_run,i_mod,nb_rmons); end
-                    tmp_onsets(tmp_onsets/pars_tr > nb_min) = [];
-                    cond = struct();
-                    cond.name     = loadreg.mod{i_mod1}.name;
-                    cond.onset    = tmp_onsets;
-                    cond.duration = loadreg.mod{i_mod1}.duration;
-                    cond.tmod     = 0;
-                    for i_mod2 = 1:length(loadreg.mod{i_mod1}.subname)
-                        cond.pmod(i_mod2).name  = loadreg.mod{i_mod1}.subname{i_mod2};
-                        cond.pmod(i_mod2).param = loadreg.mod{i_mod1}.level{i_mod2};
-                        cond.pmod(i_mod2).poly = 1;
+                    for i_cond2 = 1:length(loadcond.cond{i_cond1}.subname)
+                        cond.pmod(i_cond2).name  = loadcond.cond{i_cond1}.subname{i_cond2};
+                        cond.pmod(i_cond2).param = loadcond.cond{i_cond1}.level{i_cond2};
+                        cond.pmod(i_cond2).poly = 1;
                     end
                     job.spm.stats.fmri_spec.sess(i_run).cond(end+1) = cond;
                 end
@@ -235,7 +239,7 @@ function scan_process()
     
     %% GLM FIRST LEVEL: Contrasts
     function glm_first_contrasts()
-        if ~do_frst; return; end
+        if ~do_scnd; return; end
         jobs = {};
         for i_sub = u_subject
             fprintf('GLM first level contrasts for: subject %02i\n',i_sub);
@@ -309,6 +313,8 @@ function scan_process()
 end
 
 %% extra functions
+
+% create subdirectories
 function mkdirp(path)
     assert(~ispc(),'mkdirp: doesnt work under win');
     if path(1  )~='/'; path = [pwd(),filesep(),path]; end
@@ -320,6 +326,7 @@ function mkdirp(path)
     mkdir(path);
 end
 
+% convert binary to signed
 function y = bin2sign(x)
     l = logical(x);
     y = double(x);
@@ -327,6 +334,7 @@ function y = bin2sign(x)
     y(~l) = -1;
 end
 
+% get onset of regressors
 function reg_onset = get_regonset(timerun,u_reg)
     reg_onset = [];
     for i_reg = 1:length(u_reg)
@@ -335,12 +343,39 @@ function reg_onset = get_regonset(timerun,u_reg)
     reg_onset = sort(reg_onset);
 end
 
+% get onset of modulators
 function mod_onset = get_modonset(timerun,u_mod)
-    mod_onset = [];
+    mod_onsets = {};
     for i_mod1 = 1:length(u_mod.value)
+        mod_onset = [];
         for i_mod2 = 1:length(u_mod.value{i_mod1}.value)
             mod_onset = [mod_onset,timerun.(u_mod.value{i_mod1}.value{i_mod2})];
         end
+        mod_onsets{i_mod1} = sort(mod_onset);
     end
-    mod_onset = sort(mod_onset);
+    for i_mod = 2:length(mod_onsets)
+        assert(length(mod_onsets{i_mod-1})==length(mod_onsets{i_mod}) ,'scan_process: get_modonset: error. different onsets');
+        assert(all(  (mod_onsets{i_mod-1})==      (mod_onsets{i_mod})),'scan_process: get_modonset: error. different onsets');
+    end
+end
+
+% merge conditions
+function cond = merge_cond(cond)
+    l_cond = length(cond);
+    i_cond1 = 0;
+    i_cond2 = 0;
+    while i_cond1 < l_cond
+        i_cond1 = i_cond1 + 1;
+        i_cond2 = i_cond1;
+        while i_cond2 < l_cond
+            i_cond2 = i_cond2 + 1;
+            if strcmp(cond{i_cond1}.name,cond{i_cond2}.name)
+                assert(length(cond{i_cond1}.onset)==length(cond{i_cond1}.onset) ,'scan_process: merge_cond: error. different onsets');
+                assert(all(  (cond{i_cond1}.onset)==      (cond{i_cond1}.onset)),'scan_process: merge_cond: error. different onsets');
+                cond{i_cond1}.subname = {cond{i_cond1}.subname{:} cond{i_cond2}.subname{:}};
+                cond{i_cond1}.level   = {cond{i_cond1}.level{:}   cond{i_cond2}.level{:}  };
+                cond(i_cond2) = [];
+            end
+        end
+    end
 end
