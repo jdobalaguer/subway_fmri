dba73ad4123a84faeef2f3b1a9f0ce5579de878b
jan <jan@deepmind.com>
2014-01-14 19:48:26 +0000

    added factors to the scan_process analysis

---
+++ code/analysis/scan/scan_generatefactors.m
@@ -0,0 +1,38 @@
+
+function [timeruns,dataruns] = scan_generatefactors(datafiles)
+    
+    timeruns = {};
+    dataruns = {};
+    if ~iscell(datafiles); datafiles = {datafiles}; end
+    
+    for i_datafiles = 1:length(datafiles)
+        datafile = datafiles{i_datafiles};
+        
+        %% set breakgs
+        breakgs = unique(datafile.time.breakgs);
+        breakgs(isnan(breakgs)) = [];
+        
+        %% go through runs
+        u_break   = unique(datafile.data.exp_break);
+        nb_breaks = length(u_break);
+        
+        for i_break = 1:nb_breaks
+            ii_break = (datafile.data.exp_break==u_break(i_break));
+            % time
+            timeruns{end+1} = datafile.data.time_trial(ii_break) - breakgs(i_break);
+            % data
+            dataruns{end+1} = struct_index(datafile.data,ii_break);
+        end
+    end
+    
+end
+
+function [substruct] = struct_index(mainstruct,index)
+    substruct = mainstruct;
+    u_field = fieldnames(substruct);
+    nb_fields = length(u_field);
+    for i_field = 1:nb_fields
+        field = u_field{i_field};
+        substruct.(field)(~index) = [];
+    end
+end    
--- code/analysis/scan/scan_loadfactors.m
+++ code/analysis/scan/scan_loadfactors.m
@@ -1,5 +1,5 @@
 
-function [timeregs,dataregs] = scan_loadregressors(session)
+function [timefacs,datafacs] = scan_loadfactors(session)
     
     %% data
     % dir the 'data' folder
@@ -21,24 +21,24 @@ function [timeregs,dataregs] = scan_loadregressors(session)
     for i_lsdata = 1:nb_lsdata
         file = [lspath,lsdata{i_lsdata}];
         data = load(file);
-        fprintf('scan_loadregressors: loading datafile: %s\n',file);
+        fprintf('scan_loadfactors: loading datafile: %s\n',file);
         scans{i_lsdata} = data;
     end
     
-    %% generate regressors (screen-based)
+    %% generate factors (trial-based)
     
-    % generate regressors 
-    [timeregs,dataregs] = scan_generateregressors(scans);
+    % generate factors
+    [timefacs,datafacs] = scan_generatefactors(scans);
     
     % remove cancelled runs
     ii_remove = [];
-    for i_runs = 1:length(timeregs)
-        if length(timeregs{i_runs}.screen_block)<10
-            fprintf('scan_loadregressors: removing run %d/%d\n',i_runs,length(timeregs));
+    for i_runs = 1:length(datafacs)
+        if length(unique(datafacs{i_runs}.exp_block))<10
+            fprintf('scan_loadfactors: removing run %d/%d\n',i_runs,length(datafacs));
             ii_remove(end+1) = i_runs;
         end
     end
-    timeregs(ii_remove) = [];
-    dataregs(ii_remove) = [];
-    
+    timefacs(ii_remove) = [];
+    datafacs(ii_remove) = [];
+
 end
