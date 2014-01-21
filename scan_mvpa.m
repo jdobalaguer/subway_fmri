b8d629dd4ea4521a4672f77bfdcda5233023ff23
jan <jan@deepmind.com>
2014-01-21 05:09:36 +0000

    scan_mvpa merge runs

--- scan_mvpa.m
+++ scan_mvpa.m
@@ -1,7 +1,6 @@
 function [subj results] = scan_mvpa(i_sub)
 
     %% check
-
     % mvpa path
     paths     = strsplit(path(),':');
     mvpa_path = fileparts(which('mvpa_add_paths'));
@@ -11,26 +10,36 @@ function [subj results] = scan_mvpa(i_sub)
 
     % neural networking toolbox
     if ~exist('newff')
-        error('This tutorial requires the neural networking toolbox, if it is unavailable this will not execute');
+        %error('This tutorial requires the neural networking toolbox, if it is unavailable this will not execute');
     end
 
+    %% static variables
+    nb_runs = 4;
+    dir_sub = sprintf('data/nii/sub_%02i/',i_sub);
+    dir_img = cell(1,nb_runs);
+    for i_run = 1:nb_runs
+        dir_img{i_run} = sprintf('%sepi3/run%d/images/',dir_sub,i_run);
+    end
+    
     %% prepare
 
     % subject
-    subj_id   = sprintf('sub%02i',i_sub);
+    subj_id   = sprintf('sub%02i_run%02i',i_sub,i_run);
     subj_exp  = 'subway';
-    subj_path = '';
-    subj = init_subj(subj_id,subj_exp,subj_path,subj_id);
+    subj = init_subj(subj_id,subj_exp);
 
     % mask
-    subj_maskname = 'mask';
-    subj_maskfile = '';
+    subj_maskname = 'maskname';
+    subj_maskfile = 'maskfile';
     subj = load_afni_mask(subj,subj_maskname,subj_maskfile);
 
     % pattern
-    subj_rawfile  = {''};
+    subj_rawfile  = cellstr(spm_select('List', dir_img{1}, '^images.*\.nii$'));
+    for i_run = 2:nb_runs
+        subj_rawfile = cellcat(subj_rawfile , cellstr(spm_select('List',dir_img{i_run},'^images.*\.nii$')));
+    end
     subj_pattname = 'epi';
-    subj = load_afni_pattern(subj,subj_patname,subj_maskname,subj_rawfile);
+    subj = load_afni_pattern(subj,subj_pattname,subj_maskname,subj_rawfile);
 
     % conditions
 
@@ -65,13 +74,20 @@ function [subj results] = scan_mvpa(i_sub)
     class_args.nHidden = 0;
 
     % cross validation
-    [subj results] = cross_validation(subj,'epi_z','conds','runs_xval','epi_z_thresh0.05',class_args);
+    [subj,results] = cross_validation(subj,'epi_z','conds','runs_xval','epi_z_thresh0.05',class_args);
     
     %% save
-    save_path = sprintf('save_mvpa_sub%02i.mat',i_sub);
-    save(subj,results)
+    save_path = sprintf('save_mvpa_sub%02i_run%02i.mat',i_sub,i_run);
+    save(save_path,subj,results)
 end
 
+function c = cellcat(varargin)
+    assert(~isempty(varargin),'scan_mvpa: cellcat: error. empty varargin');
+    c = varargin{1};
+    for i = 2:length(varargin)
+        c = {c{:},varargin{i}{:}};
+    end
+end
 
 
 
