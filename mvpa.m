070abe77ac97f16948d9943680a6a11e75dc1dd8
jan <jan@deepmind.com>
2014-09-18 23:36:13 +0100

    working on the mvpa

--- mvpa.m
+++ mvpa.m
@@ -3,6 +3,9 @@ function scan = mvpa()
     %% WARNINGS
     %#ok<*NBRAK,*UNRCH>
     
+    %% DATA
+    data  = load_data_ext( 'scanner');
+    
     %% SCANNER
     scan.pars.nslices   = 32;
     scan.pars.tr        = 2;
@@ -20,34 +23,44 @@ function scan = mvpa()
     scan.glm.hrf.ord    = [0,0];
     scan.glm.image      = 'normalisation';
     scan.glm.marge      = 5;
-    scan.glm.pooling    = 1;
-    scan.glm.redo       = 1;
-    
-    %% GLM REGRESSORS
-    % todo: break regressors by event
-    data  = load_data_ext( 'scanner');
     scan.glm.regressor = struct(                                ...
           'subject', { data.expt_subject                        ... subject
         },'session', { data.expt_session                        ... session
         },'onset',   { data.vbxi_onset                          ... onset
         },'discard', { []                                       ... discard
-        },'name',    { {}                                       ... name
+        },'name',    { 'Trial'                                  ... name
         },'subname', { {}                                       ... subname
         },'level',   { {}                                       ... level
         },'duration',{ 0 });                                    ... duration
     
     %% MVPA SETTINGS
     scan.mvpa.name      = 'first_mvpa';
-    scan.mvpa.redo      = 1;
-    scan.mvpa.mask      = '';
-    scan.mvpa.partition = 4;
-    scan.mvpa.shift     = 0;
-    scan.mvpa.nn.hidden = 50;
+    scan.mvpa.image     = 'Trial';
+    scan.mvpa.source    = 'beta'; ... "beta" "cont" "spmT"
+    scan.mvpa.mask      = 'S_parietal';
+    scan.mvpa.zscore    = 0;
+    scan.mvpa.regressor = struct(                               ...
+          'subject', { data.expt_subject                        ... subject
+        },'session', { data.expt_session                        ... session
+        },'discard', { ~data.resp_bool                          ... discard
+        },'name',    { {'regular','exchange'}                   ... name
+        },'level',   { {data.vbxi_exchange_in==0,               ... level
+                        data.vbxi_exchange_in==1}               ... 
+        });
+%     scan.mvpa.classifier = struct( ...
+%           'train_funct_name', {'train_bp_netlab'    ...
+%         },'test_funct_name',  {'test_bp_netlab'     ...
+%         },'nHidden',          {100,                ...
+%         });
+    scan.mvpa.classifier = struct( ...
+          'train_funct_name', {'train_jan'  ...
+        },'test_funct_name',  {'test_jan'   ...
+        });
 
-    %% SANITY CHECK
+    %% FILL-IN
     scan = scan_initialize(scan);
      
     %% RUN
-    scan_mvpa_run(scan); 
+    scan = scan_mvpa_run(scan);
     
 end
