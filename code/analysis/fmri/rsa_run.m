11758964122dd2db79ed7c00d55e9d8f25b608b7
jan <jdobalaguer@gmail.com>
2014-11-28 17:21:14 +0000

    scan RSA

--- code/analysis/fmri/rsa_run.m
+++ code/analysis/fmri/rsa_run.m
@@ -1,7 +1,7 @@
 
 function scan = rsa_run()
     %% WARNINGS
-    %#ok<*NUSED,*ALIGN,*INUSD>
+    %#ok<*NUSED,*ALIGN,*INUSD,*NASGU>
     
     %% DATA
     block = load_block_ext('scanner');
@@ -12,46 +12,48 @@ function scan = rsa_run()
     
     %% SET SUBJECTS
     scan.subject.r = [6,10];
-%     scan.subject.u = [1:5,7:9];
-%     scan.subject.u = 1:2;
+%     scan.subject.u = [1:5];
+%     scan.subject.u = 1;
     
     %% INDEX
-    ii_back  = logical(data.resp_direction_back_any);
-    ii_null  = (~data.resp_bool);
-    ii_away  = (~ii_back & ~ii_null & data.resp_away_any);
-    ii_fill  = ( ii_back |  ii_null |  ii_away);
-    ii_forw  = (~ii_back & ~ii_null & ~ii_away);
-    ii_easy  = (ii_forw & (data.vbxi_subline_start == data.vbxi_subline_goal));
-    ii_hard  = (ii_forw & (data.vbxi_subline_start ~= data.vbxi_subline_goal));
-    ii_first = (ii_hard &  data.expt_first );
-    ii_then  = (ii_hard & ~data.expt_first );
-    ii_far   = (ii_then & (data.optm_dist_subline_goal>1));
+%     ii_back  = logical(data.resp_direction_back_any);
+%     ii_null  = (~data.resp_bool);
+%     ii_away  = (~ii_back & ~ii_null & data.resp_away_any);
+%     ii_fill  = ( ii_back |  ii_null |  ii_away);
+%     ii_forw  = (~ii_back & ~ii_null & ~ii_away);
+%     ii_easy  = (ii_forw & (data.vbxi_subline_start == data.vbxi_subline_goal));
+%     ii_hard  = (ii_forw & (data.vbxi_subline_start ~= data.vbxi_subline_goal));
+%     ii_first = (ii_hard &  data.expt_first );
+%     ii_then  = (ii_hard & ~data.expt_first );
+%     ii_far   = (ii_then & (data.optm_dist_subline_goal>1));
+    ii = (~data.expt_first & data.resp_bool);
     
     %% RSA SETTINGS
-    scan.rsa.deeye      = 15;               % remove diagonal from the regression
-    scan.rsa.distance   = 'spearman';       % "pearson", "spearman", "kendall", "dot", "mse"
+    scan.rsa.deeye      = 10;               % remove diagonal from the regression
+    scan.rsa.distance   = 'pearson';      % "pearson", "spearman", "dot", "[s]euclidean", "manhattan", "cosine"
     scan.rsa.extension  = 'img';            % GLM files
     scan.rsa.glm        = 'realignment';
     scan.rsa.image      = 'Trial';
-    scan.rsa.mask       = ''; %'xjview/occipital.img';
+    scan.rsa.mask       = 'voxs4/S_003_parietal.img';
+    scan.rsa.mni        = true;
     scan.rsa.name       = 'pilot';
-    scan.rsa.plot.rdm   = true;
-    scan.rsa.plot.subject = 1;
+    scan.rsa.plot.flag      = [1,1,1]; %[rdm,shrinked,model]
+    scan.rsa.plot.average   = true;
+    scan.rsa.plot.subject   = false;
+    scan.rsa.pooling    = false; % merge all session
     scan.rsa.redo       = 1;
     scan.rsa.regressor  = struct(                               ...
           'subject', { data.expt_subject                        ... subject
         },'session', { data.expt_session                        ... session
-        },'discard', { ~ii_far                                  ... discard
-        },'name',    { {'subline',...
-                        'response',...
-                        'switch',...
-                       }                    ... name
-        },'level',   { {data.vbxi_subline_in,...
-                        data.resp_direction_code,...
-                        data.resp_direction_switch,...
+        },'discard', { ~ii                                      ... discard
+        },'name',    { { 'switch','response',                   ... name
+                       }  ...
+        },'level',   { { data.resp_direction_switch,            ... level
+                         data.resp_direction_code,              ...
                         } ...
         });
     scan.rsa.shrink    = true;
+    scan.rsa.within    = false; % remove values between sessions
         
     %% SANITY CHECK
     scan = scan_initialize(scan);
